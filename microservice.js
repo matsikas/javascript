432dda8e426d7cb0d613a222e52ff05e7a68fbec
John Matsikas <john.matsikas@gmail.com>
2022-12-09 20:50:20 -0700

    micro 2

--- microservice.js
+++ microservice.js
@@ -10,6 +10,12 @@ function getGreeting(name) {
   return `Hello, ${name}!`;
 }
 
+// Define the greeting function
+function add(a, b) {
+    return a + b;
+  }
+  
+
 // Define the route for the greeting microservice
 app.get('/greeting/:name', (req, res) => {
   // Get the user's name from the request parameters
@@ -22,6 +28,23 @@ app.get('/greeting/:name', (req, res) => {
   res.send(greeting);
 });
 
+// Define the "/sum" endpoint
+app.get('/sum', (req, res) => {
+    // Extract the "a" and "b" query parameters from the request
+    const a = req.query.a;
+    const b = req.query.b;
+  
+    const num1 = parseInt(a);
+    const num2 = parseInt(b);
+    const num3 = num1 + num2;
+    console.log('A ' + num1 + ' B ' + num2 + ' = C ' + num3);
+  
+    const str = String(num3);
+
+    // Send the sum as the response
+    res.send(str);
+  });
+
 // Start the Express.js app and listen on port 3000
 app.listen(3000, () => {
   console.log('Greeting microservice listening on port 3000!');
@@ -30,3 +53,5 @@ app.listen(3000, () => {
 
 // You can call this microservice from the browser as follows:
 // http://localhost:3000/greeting/Dave
+
+//http://localhost:3000/sum?a=2&b=3
---
+++ openai.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+// Replace with your OpenAI API key
+const API_KEY = "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";
+
+async function generateText(prompt, model) {
+  // Set the request parameters
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${API_KEY}`
+    },
+    body: JSON.stringify({
+      prompt,
+      model
+    })
+  };
+
+  // Make the API call to the OpenAI API
+  const response = await fetch('https://api.openai.com/v1/completions', options);
+
+  // Return the generated text from the response
+  return response.json().then(response => response.choices[0].text);
+}
+
+// Generate text using the GPT-3 model
+generateText('Hello, world!', 'text-davinci-002').then(text => {
+  console.log(text);
+});
